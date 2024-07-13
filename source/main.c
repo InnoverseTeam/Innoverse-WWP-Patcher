8c9e50cacfab49d5d323c2b68b25e5f52fafea40
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-07-13 15:25:03 +0200

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,12 +1,31 @@
+/*
+ *   Copyright (C) 2021 GaryOderNichts
+ *   Copyright (C) 2022 Pretendo Network contributors
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <string.h>
 #include <mocha/mocha.h>
+
 #include <vpad/input.h>
+#include <coreinit/mcp.h>
 
 #include "patches.h"
 #include "log.h"
 
-#include "checkInkayStatus.hpp"
-
+//thanks @Gary#4139 :p
 static void write_string(uint32_t addr, const char* str)
 {
     int len = strlen(str) + 1;
@@ -19,7 +38,7 @@ static void write_string(uint32_t addr, const char* str)
 
     if (remaining > 0) {
         uint8_t buf[4];
-        Mocha_IOSUKernelRead32(addr + num, (uint32_t*)&buf);
+        Mocha_IOSUKernelRead32(addr + num, (uint32_t*)&buf, 1);
 
         for (int i = 0; i < remaining; i++) {
             buf[i] = *(str + num + i);
@@ -31,40 +50,36 @@ static void write_string(uint32_t addr, const char* str)
 
 int main(int argc, char** argv)
 {
-	int mochaResult;
-	if ((mochaResult = Mocha_InitLibrary()) != MOCHA_RESULT_SUCCESS) {
-        log("Mocha_InitLibrary() failed with code %d", mochaResult);
-    }
-	
 #ifdef DEBUG
     WHBLogUdpInit();
 #endif
 
-    log("welcome to WWP-Innoverse!");
+    log("Welcome to Innoverse-WWP-Patches!");
 
+    //check Gamepad input to maybe skip patches
     VPADStatus status;
     VPADReadError error = VPAD_READ_SUCCESS;
     VPADRead(VPAD_CHAN_0, &status, 1, &error);
 
-	if (mochaResult != MOCHA_RESULT_SUCCESS) {
-		log("WWP-Innoverse patches failed!");
-	}
-    else if (skipPatches())
+    if (status.hold & VPAD_BUTTON_ZL)
     {
-        log("WWP-Innoverse patches skipped.");
+        log("Innoverse-WWP-Patches patches skipped.");
     }
     else
     {
-		/* URL patch */
-		for (int i = 0; i < sizeof(url_patches) / sizeof(URL_Patch); i++) {
-			write_string(url_patches[i].address, url_patches[i].url);
-		}
+        if (IOSUHAX_Open(NULL) >= 0) {
+            /* URL patch */
+            for (int i = 0; i < sizeof(url_patches) / sizeof(URL_Patch); i++) {
+                write_string(url_patches[i].address, url_patches[i].url);
+            }
 
-        log("WWP-Innoverse patches completed!");
-    }
-	
-    if (mochaResult == MOCHA_RESULT_SUCCESS) {
-        Mocha_DeInitLibrary();
+            IOSUHAX_Close();
+            log("Innoverse-WWP-Patches patches succeeded!");
+        }
+        else
+        {
+            log("Innoverse-WWP-Patches patches failed!");
+        }
     }
 
 #ifdef DEBUG
