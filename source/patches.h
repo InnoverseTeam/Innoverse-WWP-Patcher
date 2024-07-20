b8ffe96874a798241b4f7c4b635917b897c19be9
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-07-20 15:02:16 +0200

    Update patches.h

--- source/patches.h
+++ source/patches.h
@@ -8,10 +8,10 @@ typedef struct URL_Patch
 } URL_Patch;
 
 static  const URL_Patch url_patches[] = {
-    {0xE2299990, "nppl-innoverse-app.alwaysdata.net"},
+    {0xE2299990, "nppl.innoverse.club"},
     
     //nim-boss .bss
-    {0xE24B8A24, "https://nppl-innoverse-app.alwaysdata.net/p01/policylist/1/1/UNK"}
+    {0xE24B8A24, "https://nppl.innoverse.club/p01/policylist/1/1/UNK"}
 };
 
 #endif
