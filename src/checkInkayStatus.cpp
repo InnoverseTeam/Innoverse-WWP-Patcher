f4fa864db0f4e1338e7c97fca04ed3b0b03f0041
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-07-24 17:42:54 +0200

    Update checkInkayStatus.cpp

--- src/checkInkayStatus.cpp
+++ src/checkInkayStatus.cpp
@@ -12,17 +12,17 @@ static int CheckInkayJson(const char* jsonBuffer) {
     cJSON* root = cJSON_Parse(jsonBuffer);
 	
     if (root == NULL) {
-		return -1;
+	return -1;
     } 
 	
 	// Get the "storageitems" object
 	cJSON* storageitems = cJSON_GetObjectItem(root, "storageitems");
 	
 	if (storageitems == NULL) {
-		// Free the root node
-		cJSON_Delete(root);
+	    // Free the root node
+	    cJSON_Delete(root);
 		
-		return -1;
+	    return -1;
 	}
 	
 	// Get the value of "connect_to_network" key
@@ -47,7 +47,7 @@ bool skipPatches() {
     char environmentPathBuffer[0x100];
     MochaUtilsStatus status;
     if ((status = Mocha_GetEnvironmentPath(environmentPathBuffer, sizeof(environmentPathBuffer))) != MOCHA_RESULT_SUCCESS) {
-		log("Failed to get the environment path. Error: %s", Mocha_GetStatusStr(status));
+	log("Failed to get the environment path. Error: %s", Mocha_GetStatusStr(status));
     }
 
 	log("Environment path is: %s, Innoverse-patcher config path is %s", environmentPathBuffer, (environmentPathBuffer + inkayConfigPath).c_str());
@@ -63,23 +63,23 @@ bool skipPatches() {
 	fseek(configFile, 0, SEEK_SET); // seek back to beginning of file
 	
 	if (size > MAX_BUFFER_SIZE) {
-		log("Innoverse-patcher config file is too large");
-		return true;
+	    log("Innoverse-patcher config file is too large");
+	    return true;
 	}
 	
 	char buffer[MAX_BUFFER_SIZE];
 	size_t bytesRead = fread(buffer, 1, MAX_BUFFER_SIZE, configFile);
 	if (bytesRead == 0) {
-		log("Innoverse-patcher config file is empty");
-		return true;
+	    log("Innoverse-patcher config file is empty");
+	    return true;
 	}
 	
 	const char* const_buffer = buffer;
 	
 	int result = CheckInkayJson(const_buffer);
 	if (result == -1) {
-		log("Innoverse-patcher config file is invalid");
-		return true;
+	    log("Innoverse-patcher config file is invalid");
+            return true;
 	}
 	
 	return !result; 
