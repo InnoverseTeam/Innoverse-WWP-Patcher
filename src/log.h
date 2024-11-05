ea675a106a76ddf574383663720d3827809490cf
cedkeChat <123301168+00cedke@users.noreply.github.com>
2024-11-05 18:50:31 +0100

    Added log-init

--- src/log.h
+++ src/log.h
@@ -4,10 +4,12 @@
 #include <whb/log.h>
 #include <whb/log_udp.h>
 
+#define log_init() do { WHBLogUdpInit(); } while(0)
 #define log(fmt, args...) do { WHBLogPrintf(fmt, ## args); } while(0)
 
-#else //DEBUG
+#else // DEBUG
 
+#define log_init() do {} while(0)
 #define log(fmt, args...) do {} while(0)
 
 #endif
